7222db4aa2f9f0e8034ebe2981dbd42603a3ffb8
wkkwkww411-ai <wkkwkww411@gmail.com>
2026-06-24 11:35:40 +0300

    Upload .devcontainer/vnc_startup.sh

---
+++ .devcontainer/vnc_startup.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+
+[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+xsetroot -solid grey
+vncconfig -iconic &
+xterm -geometry 80x24+10+10 -ls -title "XTERM" &
+startxfce4 &
